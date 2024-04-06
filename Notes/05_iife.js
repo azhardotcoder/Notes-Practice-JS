3f88018d283af7e319cea999438399ab1fe5ca24
Azhar-pc <azar123khan456@gmail.com>
2024-04-06 11:29:01 +0530

    added iife notes

---
+++ Notes/05_iife.js
@@ -0,0 +1,15 @@
+//TODO ------- IIFE ----------
+// IIFE - Immediately Invoked Function Expression
+// IIFE we use to overcome global polution
+// IIFE is a function that runs as soon as it is defined.
+// It is a design pattern which is also known as Self-Executing Anonymous Function and contains two major parts:
+
+/* //TODO-------code of iife------
+(function () {
+    console.log("Hello World");
+})(); //* Whenever we use two iffe we have to use semicolon at the end of first iffe
+
+( (name)=>{
+    console.log(`My name is ${name}`);
+} )("Azhar")
+*/
