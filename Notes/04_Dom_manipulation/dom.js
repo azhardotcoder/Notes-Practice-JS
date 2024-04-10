22e8fae3127c88ed99cd0a02b3e751f634866d01
Azhar-pc <azar123khan456@gmail.com>
2024-04-10 16:03:30 +0530

    added dom notes

---
+++ Notes/04_Dom_manipulation/dom.js
@@ -0,0 +1,27 @@
+//todo ----------------- DOM -------------------
+// * DOM stands for Document Object Model.
+// * DOM is a programming interface for web documents.
+// * DOM represents the structure of the document.
+
+//todo ----------------- DOM Manipulation -------------------
+// * DOM manipulation is the process of changing the structure of the document.
+// * DOM manipulation is used to change the content of the document.
+// * DOM manipulation is used to change the style of the document.
+// * DOM manipulation is used to change the attributes of the document.
+// * DOM manipulation is used to change the classes of the document.
+
+//todo ----------------- DOM Selectors -------------------
+// * DOM selectors are used to select the elements of the document.
+// * There are different types of selectors in DOM.
+    // * getElementById()
+    // * getElementsByClassName()
+    // * getElementsByTagName()
+    // * querySelector()
+    // * querySelectorAll()
+
+//todo ----------------- Syntax of DOM Manipulation -------------------
+document.querySelector('selector').property = 'value';
+document.getElementById("id")
+document.getElementsByClassName("class")
+
+// Diagram > Check dom manipulation folder for image
