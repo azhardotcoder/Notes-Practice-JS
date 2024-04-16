6010791d640eb464b83a2a0857156966d2bddbaa
Azhar-pc <azar123khan456@gmail.com>
2024-04-16 12:35:03 +0530

    Added async notes

---
+++ Notes/07_Async/script.js
@@ -0,0 +1,21 @@
+//todo -------------------Java Script -----------------------
+//*     1.  JavaScript is a single-threaded language, which means it has only one call stack and one memory heap.
+//*     2.  Syncronous programming: In synchronous programming, the code is executed sequentially from top-to-bottom.
+
+//todo ------------------- Execution context -----------------------
+// Execute one line of code at a time
+// 1. Global Execution Context
+// 2. Function Execution Context
+// 3. Eval Function Execution Context
+
+
+// todo ------------------ Blocking Code ---------------------
+// Blocking code: In blocking code, the code is executed sequentially from top-to-bottom.
+
+// Blocking code block the execution of the code until the current code is executed.
+
+
+//todo ----------------- Non-Blocking Code -------------------
+// Non-Blocking code: In non-blocking code, the code is executed simultaneously from top-to-bottom.
+
+// Non-blocking code does not block the execution of the code. It allows the execution of the code to continue while waiting for the response from the previous code.
