a8ce55e90ba33cedf4c287e258bcc1967b2f0f22
Azhar-pc <azar123khan456@gmail.com>
2024-04-17 13:58:58 +0530

    update 07 folder

--- Notes/07_Async/02_Notes.js
+++ Notes/07_Async/02_Notes.js
@@ -11,11 +11,19 @@
 // setTimeout() is a method that executes a function after a specified amount of time.
 // The setTimeout() method takes two parameters: a callback function and a time in milliseconds.
 // Its run only once.
+// to stop setinterval with one time of period we require setTimeout()
 
 //? ----------------- Example -------------------
  setTimeout(function() {
     console.log("Hello World! 1");
  }, 2000)
+//==================================================
+
+ setTimeout(() => {
+  clearInterval(intervalId)
+}, 2000 ); // we have to  give time in milliseconds 1 second = 1000.
+
+//? for better understanding check script file in same folder.
 
 //todo ----------------- setInterval() -------------------
 // setInterval() is a method that executes a function at specified intervals.
